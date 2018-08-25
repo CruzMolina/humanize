e12c2b21ad4b68b49a1b831c615224bdc2dd345c
freethinkingaway <cruz.adam.molina@gmail.com>
2018-08-25 10:21:42 -0700

    updated migrations deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Humanize = artifacts.require("Humanize");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(Humanize);
 };
