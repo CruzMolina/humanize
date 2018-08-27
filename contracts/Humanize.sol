0836ee3238582d0cee29b34db998446a2262acd1
freethinkingaway <cruz.adam.molina@gmail.com>
2018-08-26 21:10:30 -0700

    Humanize v2.0: contract, build, & updated tests

--- contracts/Humanize.sol
+++ contracts/Humanize.sol
@@ -9,15 +9,16 @@ of each media file on the blockchain.
 */
 contract Humanize {
 
- // string variable holding the IPFS hash on the blockchain
- string public ipfsHash;
+ // mapping for holding the IPFS hash of a given
+ // user's address on the blockchain
+ mapping (address => string) public ipfsHash;
 
- /** @dev Stores the IPFS hash returned from
-     adding a file on the IPFS network into ipfsHash.
+ /** @dev Stores the IPFS hash returned from adding a file
+     on the IPFS network into the msg.sender's ipfsHash mapping.
    * @param x The hash returned from the IPFS network.
    */
  function storeHash(string x) public {
-   ipfsHash = x;
+   ipfsHash[msg.sender] = x;
  }
 
 }
